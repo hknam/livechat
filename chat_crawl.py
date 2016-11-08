cca2051310a06e11232e1d09b2622ef7ab09d0c1
hknam <hknam0205@gmail.com>
2016-11-08 16:33:18 +0900

    timestamp add

--- chat_crawl.py
+++ chat_crawl.py
@@ -6,10 +6,31 @@ drive_path = './chromedriver'
 driver = webdriver.Chrome(drive_path)
 #os.environ["webdriver.chrome.driver"] = driver
 
-driver.get('https://www.youtube.com/watch?v=h7v_Erh6eCI')
-driver.implicitly_wait(10)
+driver.get('https://www.youtube.com/watch?v=bWqpvO8eukY')
+driver.implicitly_wait(5)
 
-comment_list = driver.find_elements_by_class_name('comment-text')
 
-for comment in comment_list:
-    print(comment, comment.text)
+'''
+while True:
+    comment_list = driver.find_elements_by_class_name('comment-text')
+    for comment in comment_list:
+        print(comment, comment.text, comment.find_elements_by_tag_name('data-timestamp'))
+
+    driver.refresh()
+    driver.implicitly_wait(5)
+'''
+
+while True:
+
+    comment_list = driver.find_elements_by_id('all-comments')
+
+    for comment in comment_list:
+        time_stamp = comment.find_element_by_css_selector('li').get_attribute('data-timestamp')
+        comment_div = comment.find_element_by_class_name('comment-text')
+        text = comment_div.text
+
+        print(time_stamp, text)
+
+    driver.refresh()
+    driver.implicitly_wait(5)
+
